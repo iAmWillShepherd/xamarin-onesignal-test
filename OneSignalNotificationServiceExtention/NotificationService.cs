2645a1d0c4d7835747160e68bcc6027871a66ee9
William Shepherd <will@codeincolor.io>
2022-10-17 14:53:14 -0500

    Xamarin state

---
+++ OneSignalNotificationServiceExtention/NotificationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using UserNotifications;
+using OneSignalSDK.Xamarin;
+
+namespace OneSignalNotificationServiceExtention
+{
+	[Register ("NotificationService")]
+	public class NotificationService : UNNotificationServiceExtension
+	{
+		Action<UNNotificationContent> ContentHandler { get; set; }
+		UNMutableNotificationContent BestAttemptContent { get; set; }
+        UNNotificationRequest ReceivedRequest { get; set; }
+
+        protected NotificationService (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void DidReceiveNotificationRequest (UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+		{
+			ReceivedRequest = request;
+			ContentHandler = contentHandler;
+			BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy ();
+
+			(OneSignal.Default as OneSignalImplementation).DidReceiveNotificationExtensionRequest(request, BestAttemptContent, ContentHandler);
+		}
+
+		public override void TimeWillExpire ()
+		{
+			(OneSignal.Default as OneSignalImplementation).ServiceExtensionTimeWillExpireRequest(ReceivedRequest, BestAttemptContent);
+			ContentHandler (BestAttemptContent);
+		}
+	}
+}
+
---
+++ testagain/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using OneSignalSDK.Xamarin;
+
+namespace testagain
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            OneSignal.Default.Initialize("8da6cb79-6938-42e9-aa3b-def99b42d8a9");
+            OneSignal.Default.PromptForPushNotificationsWithUserResponse();
+
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
